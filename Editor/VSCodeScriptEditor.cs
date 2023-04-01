cf95247efb862180cf35d06e49e5005852aaa1fb
Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>
2023-04-01 19:11:33 +1100

    ProjectGeneration handles .omnisharp, .editiorconfig, and analyzers
    No booleans.

--- Editor/VSCodeScriptEditor.cs
+++ Editor/VSCodeScriptEditor.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Diagnostics;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using Unity.CodeEditor;
 using Unity.CodeEditor;
 using UnityEditor;
 using UnityEngine;
@@ -179,26 +181,6 @@ namespace VSCodeEditor
             {
                 m_ProjectGeneration.Sync();
             }
-
-            if (!HasNuGetFolder())
-            {
-                CreateNuGetFolder();
-            }
-
-            if (!HasRoslynDLL())
-            {
-                CreateRoslynDLL();
-            }
-
-            if (!HasOmniSharpConfig())
-            {
-                CreateOmniSharpConfig();
-            }
-
-            if (!HasEditorConfig())
-            {
-                CreateEditorConfig();
-            }
         }
 
         public void SyncIfNeeded(
@@ -297,121 +279,6 @@ namespace VSCodeEditor
 
             process.Start();
             return true;
-        }
-
-        private void CreateNuGetFolder()
-        {
-            var nugetFolder = Path.Combine(m_ProjectGeneration.ProjectDirectory, "NuGet");
-            
-            Directory.CreateDirectory(nugetFolder);
-        }
-
-        private bool HasNuGetFolder()
-        {
-            var nugetFolder = Path.Combine(m_ProjectGeneration.ProjectDirectory, "NuGet");
-            return Directory.Exists(nugetFolder);
-        }
-
-        private void CreateRoslynDLL()
-        {
-            string unityRoslynDLL = Path.Combine(
-                m_ProjectGeneration.ProjectDirectory,
-                "Packages",
-                "com.tsk.ide.vscode",
-                "Editor",
-                "NuGet",
-                "Microsoft.Unity.Analyzers.dll.koala"
-            );
-
-            string nugetFolder = Path.Combine(m_ProjectGeneration.ProjectDirectory, "NuGet");
-            string nugetRoslynDLL = Path.Combine(nugetFolder, "Microsoft.Unity.Analyzers.dll");
-
-            File.Copy(unityRoslynDLL, nugetRoslynDLL);
-        }
-
-        private bool HasRoslynDLL()
-        {
-            string nugetFolder = Path.Combine(m_ProjectGeneration.ProjectDirectory, "NuGet");
-            string nugetRoslynDLL = Path.Combine(nugetFolder, "Microsoft.Unity.Analyzers.dll");
-
-            return File.Exists(nugetRoslynDLL);
-        }
-
-        private void CreateOmniSharpConfig()
-        {
-            string configFilePath = Path.Combine(
-                m_ProjectGeneration.ProjectDirectory,
-                "omnisharp.json"
-            );
-
-                File.WriteAllText(
-                    configFilePath,
-                    @"{
-    ""RoslynExtensionsOptions"": {
-        ""EnableAnalyzersSupport"": true,
-        ""LocationPaths"": [""./NuGet""]
-    },
-    ""FormattingOptions"": {
-        ""newLine"": ""\n"",
-        ""useTabs"": false,
-        ""tabSize"": 2,
-        ""indentationSize"": 2,
-
-        ""NewLinesForBracesInTypes"": false,
-        ""NewLinesForBracesInMethods"": false,
-        ""NewLinesForBracesInProperties"": false,
-        ""NewLinesForBracesInAccessors"": false,
-        ""NewLinesForBracesInAnonymousMethods"": false,
-        ""NewLinesForBracesInControlBlocks"": false,
-        ""NewLinesForBracesInAnonymousTypes"": false,
-        ""NewLinesForBracesInObjectCollectionArrayInitializers"": false,
-        ""NewLinesForBracesInLambdaExpressionBody"": false,
-
-        ""NewLineForElse"": false,
-        ""NewLineForCatch"": false,
-        ""NewLineForFinally"": false,
-        ""NewLineForMembersInObjectInit"": false,
-        ""NewLineForMembersInAnonymousTypes"": false,
-        ""NewLineForClausesInQuery"": false
-        }
-}"
-                );
-        }
-
-        private bool HasOmniSharpConfig()
-        {
-            string configFilePath = Path.Combine(
-                m_ProjectGeneration.ProjectDirectory,
-                "omnisharp.json"
-            );
-
-            return File.Exists(configFilePath);
-        }
-
-        private void CreateEditorConfig()
-        {
-            string configFilePath = Path.Combine(
-                m_ProjectGeneration.ProjectDirectory,
-                ".editorconfig"
-            );
-
-            File.WriteAllText(
-                configFilePath,
-                @"root=true
-
-[*.cs]
-dotnet_diagnostic.IDE0051.severity = none"
-            );
-        }
-
-        private bool HasEditorConfig()
-        {
-            string configFilePath = Path.Combine(
-                m_ProjectGeneration.ProjectDirectory,
-                ".editorconfig"
-            );
-
-            return File.Exists(configFilePath);
         }
 
         static bool SupportsExtension(string path)
